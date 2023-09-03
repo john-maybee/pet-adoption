0df00ce9dac90e15044941b1c5e1a4e25b9b8dc1
John Maybee <john.d.maybee@gmail.com>
2023-09-02 21:49:10 -0400

    detailed how the useQuery hook works within the code comments

--- adoptables/src/Details.jsx
+++ adoptables/src/Details.jsx
@@ -4,11 +4,11 @@ import fetchPet from "./fetchPet";
 
 const Details = () => {
     const { id } = useParams();
-    const results = useQuery(["details", id], fetchPet);
+    const results = useQuery(["details", id], fetchPet); // useQuery hook takes in a key and a function that returns a promise (fetchPet) and returns an object with properties. This can replace the useEffect hook in usable components.
 
     if (results.isError) {
         return <h2>ohno! There is an error with the results.</h2>;
-    }
+    } 
 
     if (results.isLoading) {
         return (
