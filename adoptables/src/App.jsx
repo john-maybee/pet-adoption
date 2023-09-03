70b0b20f2262548e5f93c2f7ec84fe5589cb7dff
John Maybee <john.d.maybee@gmail.com>
2023-09-02 21:47:03 -0400

    refactored Details to include fetchPet and introduced the concept of useQuery to replace useEffect

---
+++ adoptables/src/App.jsx
@@ -0,0 +1,36 @@
+import {createRoot} from "react-dom/client";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity, 
+      cacheTime: Infinity,
+    },
+  },
+});
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
+    </BrowserRouter>
+  );
+};
+
+
+const container = document.getElementById("root");
+const root =  createRoot(container);
+// root.render(React.createElement(App)); // this is the same as below but using JSX instead of React.createElement through Vite transpiling
+root.render(<App />);
---
+++ adoptables/src/Pet.jsx
@@ -0,0 +1,36 @@
+// This is an example of a jsx component and cannot be named Pet.js because it is not a simple JavaScript React component but a jsx component that needs to be transpiled by Vite to be used in the browser.
+
+// import React from "react";
+
+// const Pet = (props) => {
+//     return React.createElement("div", {}, [
+//       React.createElement("h1", {}, props.name),
+//       React.createElement("h2", {}, props.animal),
+//       React.createElement("h2", {}, props.breed),
+//     ]);
+//   };
+
+// below is the same as above but using JSX instead of React.createElement through Vite transpiling
+import { Link } from "react-router-dom";
+
+const Pet = ({name, animal, breed, images, location, id}) => {
+    let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+    if (images.length) {
+        hero = images[0];
+    }
+
+    return (
+        <Link to ={`/details/${id}`} className="pet">
+            <div className="image-container">
+                <img src={hero} alt={name} />
+            </div>
+            <div className="info">
+                <h1>{name}</h1>
+                <h2>{`${animal} - ${breed} - ${location}`}</h2>
+            </div>
+        </Link>
+
+    );
+}; // end of Pet component function
+
+  export default Pet;
